7ce83d0dd3eae0076dbeb68a42f06f7796a08649
Damien Hauser <damien@dhconsulting.ch>
2026-03-14 13:20:39 +0100

    change settings

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ def create_app() -> Flask:
 
     login_manager = LoginManager()
     login_manager.login_view = "login"
+    login_manager.login_message = None
     login_manager.init_app(app)
 
     @login_manager.user_loader
